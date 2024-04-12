6d2f4af2b7e5d0153db0b3d50a62999a039b7816
dellyu03 <seunghwan081@gmail.com>
2024-04-12 23:34:10 +0900

    feat:make main page change by goal

--- JS/index.js
+++ JS/index.js
@@ -1,55 +1,88 @@
 const userName = "유승환";
-const dday = new Date('2024-08-13');
-const fullday = 547;
-
+//하루의 시간을 밀리세크로 나타냄
 const oneDay = 24 * 60 * 60 * 1000;
-const goal = "전역";
-const statusDate = document.getElementById("pointDay");
-const welcomeMsg = document.getElementById("welcomeMsg");
-const statusPercentage = document.getElementById("percentage");
-const statusProgress = document.getElementById("percentage_progress");
+const now = new Date ();
 
-
-var left = calDDay (dday);
-var goalDateList = dateFormat(dday);
-const doneday = 547-left;
-var goalPercentage = doneday/fullday * 100;
-
-var welcomeString = `${userName}님! ${goal}까지 ${left}일 남았습니다!`
-var goalDate = `${goalDateList[0]}.${goalDateList[1]}.${goalDateList[2]}`
-
-console.log (goalPercentage)
-
-
-
-/** Date 데이터를 [년, 월, 일, 요일]로 리턴 */
-function dateFormat(date) {
-    var year = date.getFullYear();
-    var month = date.getMonth()+1;
-    var formatdate = date.getDate();
-    var day = date.getDay();
-
-    //요일을 문자로 나타냄
-    const weekday = ["일", "월", "화", "수", "목", "금", "토"]
-    var newdate = weekday[day];
-
-    dateList = [year, month, formatdate, newdate]
-    return (dateList)
+let goal1 = {
+    name : "전역",
+    startDate : "2023.02.14",
+    endDate : "2024.08.13"
 }
 
-/** 현재 시간에서 D-Day까지 남은 일수를 계산 */
-function calDDay(dday) {
-   const now = new Date();
-   var timeGap = dday - now;
-   var remainTime = Math.ceil(timeGap/oneDay);
-   return (remainTime);
+let goal2 = {
+    name : "병장 진급",
+    startDate : "2023.11.01",
+    endDate : "2024.05.01"
+}
+
+/**알맞은 형식으로 받은 날짜 정보를 받아 [년, 월, 일, 요일] 리스트르 리턴하는 함수 */
+function formatDate (input) {
+    inputDate = new Date(input);
+    year = inputDate.getFullYear();
+    month = inputDate.getMonth();
+    date = inputDate.getDate();
+    day = inputDate.getDay();
+
+    result = [year, month, date, day];
+    return(result);
+}
+
+/**firstDate에서 secondDate까지 남은 일수를 리턴하는 함수 */
+function calDiff(firstDate, secondDate) {
+    var startDate =new Date(firstDate);
+    var endDate = new Date (secondDate);
+    
+    var timeGap = endDate - startDate;
+    var daydiff = Math.ceil(timeGap/oneDay);
+    
+    return (daydiff);
+ }
+
+ /**secondDate까지 얼마만큼 수행 했는지 퍼센티지로 리턴하는 함수 */
+function calPercentage(firstDate, secondDate){
+    var fullDay = calDiff(firstDate, secondDate);
+    var doneday = calDiff(firstDate, now);
+    var remainDay = calDiff(now,secondDate);
+    
+    var percentage = doneday/fullDay* 100;
+    var percentage = percentage.toFixed(1);
+
+    return(percentage);
+    
 }
 
 
-//웰컴 메시지를 출력
-welcomeMsg.innerHTML = welcomeString
+ /**goal까지 전체 일수, 남은 일수, 지금까지 달성한 퍼센티지를 리스트로 리턴하는 함수 */
+function goalToDay(goal) {
+    var goalStart = goal.startDate;
+    var goalEnd = goal.endDate;
 
-//목표일을 스테이터스바에 표시
-statusDate.innerHTML = goalDate
+    var fullDay = calDiff(goalStart, goalEnd);
+    var remainDay = calDiff(now, goalEnd);
+    var percentage = calPercentage(goalStart, goalEnd);
+    
+
+    result = [fullDay, remainDay, percentage]
+    return (result);
+}
+
+//아래부터는 실제 HTML 조작하는 코드
+const domwelcome = document.getElementById("welcomeMsg")
+const dompercentage = document.getElementById("percentage")
+const domprogress = document.getElementById("percentage_progress");
+const domgoalName = document.getElementById("goalName");
+const domgoalDay = document.getElementById("goalDay");
+var selectedGoal = goal2;
 
 
+/** welcome 메시지 작성 코드 */
+function crWelcomeMsg (goal) {
+    msg = `${userName}님 "${goal.name}"까지 ${goalToDay(goal)[1]}일 남았습니다!`
+    return (msg);
+}
+
+domprogress.value = goalToDay(selectedGoal)[2]
+domwelcome.innerHTML = crWelcomeMsg(selectedGoal);
+dompercentage.innerHTML = `${goalToDay(selectedGoal)[2]}%`;
+domgoalName.innerHTML = selectedGoal.name;
+domgoalDay.innerHTML = selectedGoal.endDate;
