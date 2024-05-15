c74f431e0fc778af356d9454faf1ec33cc26143d
dellyu03 <seunghwan081@gmail.com>
2024-05-15 23:45:17 +0900

    edit test css router

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.listen(port, () => {
 
 //static 파일안에 든 정적 파일을 가져오는 코드
 app.use('/', express.static('JS'),express.static('public'));
+app.use('/test', express.static('JS'),express.static('public'));
+
 
 
 //HTML은 잘 전달 되지만 CSS는 전달이 안됨 static 정적파일 끌어오는 걸로 해결 됨
