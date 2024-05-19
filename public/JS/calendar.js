47368c0b5f54600b99d8be5cba1290b63e5bc1f6
dellyu03 <seunghwan081@gmail.com>
2024-05-19 23:49:14 +0900

    마지막 날짜 구하는 함수 구현

--- public/JS/calendar.js
+++ public/JS/calendar.js
@@ -7,8 +7,11 @@ function getCurDay () {
 //
 
 //현재 달의 마지막날이 몇일인지 구하는 함수
-function isBigMon (year, mon) {
-    
+function getLastDay (year, mon) {
+    date = new Date(`${year} ${mon}`);
+    date.setDate(0);
+    lastDay = date.getDate();
+    return lastDay;
 };
 
 //전달과 다음달을 구하는 함수
@@ -21,3 +24,16 @@ function getNearMon () {
 function createCalendar() {
 
 }
+
+//달력 제목에 접근해서 날짜로 변화하는 함수
+function getCalendarTitle() {
+    const calendarTitle = document.getElementById("calendar__title");
+    
+    //split strings by space
+    data = calendarTitle.innerText;
+    data = data.split(' ');
+    
+    return data;
+}
+
+console.log(getLastDay(2024, 05))
